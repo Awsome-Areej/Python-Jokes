4c0aad18e0cbda877bc9ac9dc8af59bf4219da21
Awsome-Areej <41355424+Awsome-Areej@users.noreply.github.com>
2018-07-21 22:38:41 -0700

    Create Knock_knock_joke.py

---
+++ Knock_knock_joke.py
@@ -0,0 +1,33 @@
+def joke():
+    for joke in range (4):
+        whothere = input("Knock knock")
+        if whothere == "Who is there":
+            banana = input("Banana")
+            if banana == "Banana who":
+                print "         "
+            else:
+                print "Please type in Banana who"
+                banana = input ("Banana")
+        else:
+            print "Please print in Who is there"
+            whothere = input ("Knock knock")
+            banana = input("Banana")
+            if banana == "Banana who":
+                print "         "
+            else:
+                print "Please type in Banana who"
+                banana = input ("Banana")
+    whoisit = input ("Knock knock")
+    if whoisit == "Who is there":
+        orange = input ("Orange"
+        if orange == "Orange who":
+            print "Orange you glad I didn't say banana!"
+        else:
+            print "Restart please...."
+            print "The whole code!!!!"
+            print "                  "
+    else:
+        print "Restart please...."
+        print "The whole code!!!!"
+        print "                  "
+        
